b96017d05d21dae6d04c1128523f037b8109e4d8
TImoteoVieira <timoteo7charles777@gmail.com>
2022-09-01 13:43:51 -0300

    [add]: projeto Planilhas Google

---
+++ src/index.js
@@ -0,0 +1,63 @@
+const credentials = require('./credentials.json')
+const {GoogleSpreadsheet} = require('google-spreadsheet')
+require('dotenv').config()
+const spreadsheetId = process.env.SHEET_ID
+
+const doc = new GoogleSpreadsheet(spreadsheetId)
+
+async function auth() {
+  await doc.useServiceAccountAuth(credentials)
+  await doc.loadInfo()
+  return doc
+} 
+
+// validando autenticação e imprimindo o titulo da planilha
+auth().then(doc => {
+  console.log(doc.title);
+});
+
+//adicionando colunas a tabela
+auth().then(doc => {
+  const sheet = doc.sheetsByIndex[0]
+  sheet.setHeaderRow(['nome', 'idade', 'email'])
+  .then(() => {
+      console.log('Colunas adicionadas')
+  })
+});
+
+//adicionando valores a tabela
+auth().then(doc => {
+    const sheet = doc.sheetsByIndex[0];
+    sheet.addRow({
+        nome: "Timóteo",
+        idade: 25,
+        email: "timoteo@gmail"
+    }).then(() => {
+        console.log('Valores da coluna adicionadas')
+    })
+});
+
+//listando valores da coluna 'nome'
+auth().then(doc => {
+    const sheet = doc.sheetsByIndex[0];
+    sheet.getRows().then(rows => {
+        rows.map(row => {
+            console.log(row.nome);
+        })
+    })
+})
+
+//deletando valores da coluna
+auth().then(doc => {
+  sheet = doc.sheetsByIndex[0];
+  sheet.getRows().then(rows => {
+      rows.map(row => {
+          if(row.nome === "Timóteo"){
+              row.delete().then(() => {
+                  console.log('Valor do campo nome deletado');
+              });
+          }
+      });
+  })
+})
+
